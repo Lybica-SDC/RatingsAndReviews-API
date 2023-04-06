f709bd8ff99806c35a55c229300e2597d02bf362
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-06 15:46:52 -0700

    modify schemas and transfer all CSV data into the database

--- database/ETL.js
+++ database/ETL.js
@@ -1,25 +1,10 @@
 const path = require('path');
 const db = require('../db.js')
-// console.log(__dirname);
-// const transferReviews = async () => {
-//   const filePath = path.resolve(__dirname, './data/reviews.csv');
-//   console.log('file name: ', filePath);
-//   const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${filePath}' DELIMITER ',' CSV HEADER;`
 
-//   try {
-//     await db.none(queryString);
-//     console.log('transfer reviews data complete')
-//   } catch (err) {
-//     console.log('error transfering reviews data: ', err);
-//   }
-// }
-
-// transferReviews();
-
-const transferCharacteristics = async () => {
-  const characteristicsPath = path.resolve(__dirname, './data/characteristics.csv');
-  console.log('file name: ', characteristicsPath);
-  const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${characteristicsPath}' DELIMITER ',' CSV HEADER;`
+const transferIntoReviews = async () => {
+  const filePath = path.resolve(__dirname, './data/reviews.csv');
+  console.log('file name: ', filePath);
+  const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${filePath}' DELIMITER ',' CSV HEADER;`
 
   try {
     await db.none(queryString);
@@ -29,28 +14,49 @@ const transferCharacteristics = async () => {
   }
 }
 
-// const transferReviews = async () => {
-//   const filePath = path.resolve(__dirname, './data/reviews.csv');
-//   console.log('file name: ', filePath);
-//   const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${filePath}' DELIMITER ',' CSV HEADER;`
+const transferIntoPhotos = async () => {
+  //create a path to the photos CSV
+  const photosPath = path.resolve(__dirname, './data/reviews_photos.csv');
+  console.log('file name: ', photosPath);
+  const queryString = `COPY photos(id, review_id, url) FROM '${photosPath}' DELIMITER ',' CSV HEADER;`
 
-//   try {
-//     await db.none(queryString);
-//     console.log('transfer reviews data complete')
-//   } catch (err) {
-//     console.log('error transfering reviews data: ', err);
-//   }
-// }
+  try {
+    await db.none(queryString);
+    console.log('transfer photos data complete')
+  } catch (err) {
+    console.log('error transfering reviews data: ', err);
+  }
+}
 
-// const transferReviews = async () => {
-//   const filePath = path.resolve(__dirname, './data/reviews.csv');
-//   console.log('file name: ', filePath);
-//   const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${filePath}' DELIMITER ',' CSV HEADER;`
+const transferCharacteristicReviews = async () => {
+  //create a path to characteristic reviews
+  const charReviewPath = path.resolve(__dirname, './data/characteristic_reviews.csv');
+  console.log('file name: ', charReviewPath);
+  const queryString = `COPY char_reviews(id,characteristic_id,review_id,value) FROM '${charReviewPath}' DELIMITER ',' CSV HEADER;`
 
-//   try {
-//     await db.none(queryString);
-//     console.log('transfer reviews data complete')
-//   } catch (err) {
-//     console.log('error transfering reviews data: ', err);
-//   }
-// }
+  try {
+    await db.none(queryString);
+    console.log('transfer charReviews data complete')
+  } catch (err) {
+    console.log('error transfering charReviews data: ', err);
+  }
+}
+
+const transferCharacteristics = async () => {
+  //create a path to characteristics
+  const characteristicPath = path.resolve(__dirname, './data/characteristics.csv');
+  console.log('file name: ', characteristicPath);
+  const queryString = `COPY characteristics(id,product_id,name) FROM '${characteristicPath}' DELIMITER ',' CSV HEADER;`
+
+  try {
+    await db.none(queryString);
+    console.log('transfer characteristics data complete')
+  } catch (err) {
+    console.log('error transfering characteristics data: ', err);
+  }
+}
+
+// transferIntoReviews();
+// transferCharacteristics();
+// transferIntoPhotos();
+transferCharacteristicReviews();
--- database/schema.sql
+++ database/schema.sql
@@ -3,52 +3,75 @@ CREATE TABLE "reviews" (
 	"id" serial NOT NULL,
 	"product_id" integer NOT NULL,
 	"rating" integer NOT NULL,
-	"date" text NOT NULL,
+	"date" VARCHAR(255) NOT NULL,
 	"summary" text NOT NULL,
 	"body" text NOT NULL,
 	"recommend" BOOLEAN NOT NULL,
 	"reported" BOOLEAN NOT NULL,
-	"reviewer_name" text NOT NULL,
-	"reviewer_email" text NOT NULL,
-	"response" text NOT NULL,
+	"reviewer_name" VARCHAR(255) NOT NULL,
+	"response" VARCHAR(255) NOT NULL,
 	"helpfulness" integer NOT NULL,
-	CONSTRAINT "Reviews_pk" PRIMARY KEY ("id")
+	"reviewer_email" VARCHAR(255) NOT NULL,
+	CONSTRAINT "reviews_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
+
+
 
 CREATE TABLE "photos" (
 	"id" integer NOT NULL,
-	"URL" text NOT NULL,
+	"url" text NOT NULL,
 	"review_id" integer NOT NULL,
-	CONSTRAINT "Photos_pk" PRIMARY KEY ("id")
+	CONSTRAINT "photos_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- CREATE TABLE "public.Product" (
--- 	"id" serial NOT NULL,
--- 	CONSTRAINT "Product_pk" PRIMARY KEY ("id")
--- ) WITH (
---   OIDS=FALSE
--- );
 
-CREATE TABLE "meta" (
-	"id" integer NOT NULL,
+
+CREATE TABLE "char_reviews" (
+	"id" serial NOT NULL,
+	"review_id" integer NOT NULL,
+	"characteristic_id" integer NOT NULL,
+	"value" integer NOT NULL,
+	CONSTRAINT "char_reviews_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "characteristics" (
+	"id" serial NOT NULL,
 	"product_id" integer NOT NULL,
 	"name" text NOT NULL,
-	"value" integer NOT NULL,
-	"characteristic_id" integer NOT NULL,
-	CONSTRAINT "Meta_pk" PRIMARY KEY ("id")
+	CONSTRAINT "characteristics_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- ALTER TABLE "Reviews" ADD CONSTRAINT "Reviews_fk0" FOREIGN KEY ("product_id") REFERENCES "Product"("id");
+
+
 
 ALTER TABLE "photos" ADD CONSTRAINT "photos_fk0" FOREIGN KEY ("review_id") REFERENCES "reviews"("id");
 
+ALTER TABLE "char_reviews" ADD CONSTRAINT "char_reviews_fk0" FOREIGN KEY ("review_id") REFERENCES "reviews"("id");
+
+ALTER TABLE "char_reviews" ADD CONSTRAINT "char_reviews_fk1" FOREIGN KEY ("characteristic_id") REFERENCES "characteristics"("id");
 
 
---create index
+
+
+
+
+
+
+
+
+
+
+
+
+
 
