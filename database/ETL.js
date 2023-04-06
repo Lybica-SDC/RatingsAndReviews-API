222e8880c49c97326c6dce28a03272e16b6ecb44
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-06 11:16:52 -0700

    write the first etl script

--- database/ETL.js
+++ database/ETL.js
@@ -1,6 +1,18 @@
-const path = require('node:path');
-console.log(__dirname);
-// const transferReviews = async () => {
+const path = require('path');
+// console.log(__dirname);
+const transferReviews = async () => {
+  const filePath = path.resolve(__dirname, './data/reviews.csv');
+  console.log('file name: ', fileName);
+  const queryString = `COPY reviews(id,product_id,rating,date,summary,body,recommend,reported,reviewer_name,reviewer_email,response,helpfulness) FROM '${filePath}' DELIMITER ',' CSV HEADER;`
+
+  try {
+    await db.none(queryString);
+    console.log('transfer reviews data complete')
+  } catch (err) {
+    console.log('error transfering reviews data: ', err);
+  }
 
 
-// }
+}
+
+transferReviews();
