df286de0f97ebebd8279b206e874ecadc9a66cea
Nam Nguyen <nam.nguyen201@gmail.com>
2023-04-06 20:11:02 -0700

    finish etl

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 require('dotenv').config();
 const express = require('express');
+const router = express.Router();
 const path = require('path');
 // const router = require('./routes');
-const db = require('./db.js')
+const db = require('./database/db.js')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../../Lybica-Client/dist')));
-// app.use('/products', router.products);
+
+//GET route
+router.get('/', (req, res) => {
+  res.json(response);
+})
+
+//POST route
+router.post('/', (req, res) => {
+  res.sendStatus(201);
+})
+
+//PUT route
+router.put('/', (req, res) => {
+  res.sendStatus(204);
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port);
